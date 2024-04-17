d9561442731a7e94ca886968ecc2eed4e29b788c
Agnethe Florentsen <agfl0001@stud.kea.dk>
2024-04-17 12:13:56 +0200

    azure

---
+++ src/main/java/com/example/wishlist/repository/LanguageRepository.java
@@ -0,0 +1,12 @@
+package com.example.wishlist.repository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+@Repository
+public class LanguageRepository {
+    @Value("${spring.datasource.url}")
+    private String db_url;
+    @Value("${spring.datasource.username")
+    private String username;
+    @Value("${spring.datasource.password")
+    private String pwd;
+}
